b001759582ed8e1f325bb9c468b201b54b756bbc
Byoung Hoon <69192178+lee040118@users.noreply.github.com>
2021-04-30 22:32:37 +0900

    Create Create_data.sh

---
+++ Create_data.sh
@@ -0,0 +1,3 @@
+python Create_data.py --source_file ./data/valid.json --source_file2 ./data/valid_qa.jsonl
+python Create_data.py --source_file ./data/test.json --source_file2 ./data/test_qa.jsonl
+python Create_data.py --source_file ./data/train.json --source_file2 ./data/train_qa.jsonl
